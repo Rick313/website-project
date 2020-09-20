abe9e3401bc26eb4d4701009b93e96b85f0642c4
RAFAIDEEN Richard <richard.rafadeen@gmail.com>
2020-09-20 18:23:34 +0200

    [Client] - styling home page

--- projects/client/src/pages/home/home.module.ts
+++ projects/client/src/pages/home/home.module.ts
@@ -1,15 +1,20 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule, Routes } from "@angular/router";
 
-import { CardModule } from '@shared/components';
+import { CardModule } from "@shared/components";
+import { ButtonModule } from "@shared/directives";
+import { HomeComponent } from "./home.component";
 
-import { HomeComponent } from './home.component';
-
-const routes: Routes = [{ path: '', component: HomeComponent }];
+const routes: Routes = [{ path: "", component: HomeComponent }];
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, RouterModule.forChild(routes), CardModule],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(routes),
+    ButtonModule,
+    CardModule,
+  ],
 })
 export class HomeModule {}
