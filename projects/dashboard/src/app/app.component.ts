65cfd9aa8f000adce5e61a314c544b09b7952355
RAFAIDEEN Richard <richard.rafadeen@gmail.com>
2020-09-23 20:48:50 +0200

    [Dashboard] - dashboard started

--- projects/dashboard/src/app/app.component.ts
+++ projects/dashboard/src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormBuilder, Validators } from "@angular/forms";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'rx-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  selector: "rx-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
-  title = 'dashboard';
+  title = "dashboard";
+  user$: Observable<any>;
+  authForm: FormBuilder;
+
+  constructor(private readonly formBuilder: FormBuilder) {
+    this.user$ = new Observable();
+
+    this.formBuilder.group({
+      email: ["", [Validators.email, Validators.required]],
+      password: ["", [Validators.required]],
+    });
+  }
+  register() {}
 }
