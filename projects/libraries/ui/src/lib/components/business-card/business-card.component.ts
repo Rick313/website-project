b2190635b83148f721d3aac72c16f925c5c42ab3
RAFAIDEEN Richard <richard.rafaideen@gmail.com>
2020-09-27 14:35:27 +0200

    [Libraries] - Create business card component

--- projects/libraries/ui/src/lib/components/business-card/business-card.component.ts
+++ projects/libraries/ui/src/lib/components/business-card/business-card.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from "@angular/core";
+
+export interface BusinessData {
+  id: string;
+  name: string;
+  description: string;
+  picture: string;
+  button: {
+    label: string;
+    click: (data: { id: string; name: string }) => void;
+  };
+}
 
 @Component({
-  selector: 'rx-business-card',
-  templateUrl: './business-card.component.html',
-  styleUrls: ['./business-card.component.scss']
+  selector: "rx-business-card",
+  template: `
+    <img [src]="data.picture" [alt]="'img-' + data.name" />
+    <p>{{ data.description }}</p>
+    <button
+      type="button"
+      (click)="data.button.click({ id: data.id, name: data.name })"
+    >
+      {{ data.button.label }}
+    </button>
+  `,
+  styleUrls: ["./business-card.component.scss"],
 })
 export class BusinessCardComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  @Input("data") data: BusinessData;
+  constructor() {}
+  ngOnInit(): void {}
 }
--- projects/libraries/ui/src/lib/components/business-card/business-card.module.ts
+++ projects/libraries/ui/src/lib/components/business-card/business-card.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from "@angular/core";
-import { CommonModule } from "@angular/common";
-import { BusinessCardComponent } from "./business-card.component";
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { BusinessCardComponent } from './business-card.component';
+
+
 
 @NgModule({
   declarations: [BusinessCardComponent],
-  imports: [CommonModule],
-  exports: [BusinessCardComponent],
+  imports: [
+    CommonModule
+  ],
+  exports: [BusinessCardComponent]
 })
-export class BusinessCardModule {}
+export class BusinessCardModule { }
